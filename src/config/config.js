057b8a6570874d0c0bbe82c5bc696deb1425ae32
luotongxiao <57798517+luotongxiao@users.noreply.github.com>
2021-08-29 06:28:36 +0800

    Delete config.js

--- src/config/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-module.exports = {
-    account: process.env.account,
-    password: process.env.password,
-    address: process.env.address,
-    temperature: process.env.temperature
-}
