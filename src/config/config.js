5d862e3f44318f21b90d727655ee94effeaf0ad1
luotongxiao <57798517+luotongxiao@users.noreply.github.com>
2021-08-29 06:15:34 +0800

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
     account: process.env.account,
-    password: ${{os.environ.password}},
+    password: process.env.password,
     address: process.env.address,
     temperature: process.env.temperature
 }
