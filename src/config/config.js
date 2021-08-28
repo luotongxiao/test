3ea531f0fed22e41cf89d778c1518e9d8c5dc01f
luotongxiao <57798517+luotongxiao@users.noreply.github.com>
2021-08-29 06:30:20 +0800

    Add files via upload

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+module.exports = {
+    account: process.env.account,
+    password: process.env.password,
+    address: process.env.address,
+    temperature: process.env.temperature
+}
